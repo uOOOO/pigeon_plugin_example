f2205017d59496d736377f2dde1b568299c1cb5e
SeungHun Choe <13363349+uOOOO@users.noreply.github.com>
2021-08-13 17:55:40 +0900

    Update dependencies

--- lib/pigeon_plugin.dart
+++ lib/pigeon_plugin.dart
@@ -5,17 +5,17 @@ import 'package:flutter/services.dart';
 import 'messages.dart';
 
 class PigeonPlugin {
-  static ExampleApi _apiInstance;
+  static ExampleApi? _apiInstance;
 
-  static ExampleApi get _api {
+  static ExampleApi? get _api {
     if (_apiInstance == null) {
       _apiInstance = ExampleApi();
     }
     return _apiInstance;
   }
 
-  static Future<String> get platformVersion async {
-    Version version = await _api.getPlatformVersion();
+  static Future<String?> get platformVersion async {
+    Version version = await _api!.getPlatformVersion();
     return version.string;
   }
 }
